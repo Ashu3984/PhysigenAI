a7b17d22ccd77df850c552af957edf1af7648fd7
SK Md Shahid <104886062+Sk4467@users.noreply.github.com>
2023-12-18 02:10:46 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,13 +1,2 @@
 
-def get_chat_completion(prompt, model="gpt-3.5-turbo"):
-    try:
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt}
-            ]
-        )
-        return response['choices'][0]['message']['content']
-    except Exception as e:
-        return str(e)
+haha
